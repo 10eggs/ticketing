ab10f8d36b4c5632f8877242ec2aaa0257cb1169
10eggs <pawlak325@gmail.com>
2022-09-07 14:45:39 +0200

    express-async-errors packaged added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import 'express-async-errors'
 import { json } from 'body-parser'
 import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin';
@@ -15,6 +16,8 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
+//There are differences between how express handling async errors. We are handling it by using express-async-errors.
+//It doesn't require to use next(), also we need to import this package only in one place in our code, do not need to use it in handlers
 //Express recognize number of arguments - if it has more than 3 arguments than it is treated as error handler
 app.all('*',()=>{
   throw new NotFoundError();

08c8cc84cf0e43ed69ddd0dcfee4a42cda6e60f7
10eggs <pawlak325@gmail.com>
2022-10-06 16:06:56 +0200

    Adding custom _app.js for next, adding bootstrap

---
+++ client/pages/_app.js
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+//Export react component
+//Whenever you try to navigate to some distinct page with Next.js,
+//Next is going to import your component from one of these files from client directory
+//Next does not just take your component and show it on the screen
+//It wraps it up in its own custom default component
+//In next this component is refered to as an 'app'
+//However we create our own custom component, _app.js
+
+//So route looks like this:
+//1. Navigate to route,
+//2. Next is importing this component (if you are navigating to root, it takes root) and it's going to pass it into our custom default component (_app) as
+//'Component' prop
+//By using this we can just import our global css component into one file, so bootstrap only imported to _app.js
+
+export default ({Component, pageProps }) =>{
+  return <Component {...pageProps}/>
+}
+
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,5 @@
 const test = () => {
-return <h1>CLIENT</h1>;
+return <h1>CLIENT4</h1>;
 }
   
 export default test;

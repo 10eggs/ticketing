2fd428306b7c065f7f42e7e95566f62775a1e906
10eggs <pawlak325@gmail.com>
2022-10-09 13:25:54 +0200

    Check on source of request - browser/server

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,32 @@
-const test = () => {
-return <h1>LANDING</h1>;
+ import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  //Browser
+  // console.log('I am in the component ', object provided by getInitialProps)
+  console.log(currentUser);
+  
+  return <h1>LANDING</h1>;
+}
+
+//Specific for Next.js
+//While defined, Next.JS is going to call this function while it's attempting to render our application on the server
+//Opportunit to fetch some data
+//Retrieved data is provided to our component as an object
+//We can't do any data loading on the component itself
+//On initial load our components are rendered only once, so there is no option to fetch anything in component
+LandingPage.getInitialProps = async () =>{
+  if(typeof window === 'undefined'){
+    //We are o the server!
+    //Request should be made to http://ingress-nginx.ingres-snginx.~~
+    const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser');
+    return data;
+  }
+  else{
+    //We are on the browser!
+    //Request can be made with a base url of ''
+    const {data} = await axios.get('/api/users/currentuser')
+    return data;
+  }
 }
   
-export default test;
+export default LandingPage;

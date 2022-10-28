16ab51d15390a602e8676d312159805043a264a9
10eggs <pawlak325@gmail.com>
2022-10-28 16:37:40 +0200

    Building generic events scope

---
+++ nats-test/src/events/ticket-created-listener.ts
@@ -0,0 +1,15 @@
+import { Message } from 'node-nats-streaming';
+import { Listener } from './base-listener';
+import { TicketCreatedEvent } from './ticket-created-event'
+import { Subjects } from './subjects';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  
+  //Why type annotation?
+  //TS may think that we'll want to change value of subject some time in the future
+  readonly subject: Subjects.TicketCreated = Subjects.TicketCreated; 
+  queueGroupName = 'payments-service';
+
+  onMessage(data: any, msg: Message){
+    console.log(`Event data in TicketCreated Listener / Q Group payment-service. Data: ${JSON.stringify(data)}`);
+    msg.ack();
+}};
